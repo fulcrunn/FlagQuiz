3425d2b8592e13676d51dacace7b4390fdeac988
ianBAlmeida <bailoneian@gmail.com>
2025-09-02 21:44:07 -0300

    Lógica inicial do quiz

--- app/src/main/java/br/com/flagquiz/QuizActivity.kt
+++ app/src/main/java/br/com/flagquiz/QuizActivity.kt
@@ -7,6 +7,26 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class QuizActivity : AppCompatActivity() {
+
+    data class Flag(val drawbleId: Int, val countryName: String)
+
+    private val flags = listOf(
+        Flag(R.drawable.flag_catar, "CATAR"),
+        Flag(R.drawable.flag_canada, "CANADA"),
+        Flag(R.drawable.flag_china, "CHINA"),
+        Flag(R.drawable.flag_egito, "EGITO"),
+        Flag(R.drawable.flag_belgica, "BELGICA"),
+        Flag(R.drawable.flag_brasil, "BRASIL"),
+        Flag(R.drawable.flag_india, "INDIA"),
+        Flag(R.drawable.flag_colombia, "COLOMBIA"),
+        Flag(R.drawable.flag_nepal, "NEPAL"),
+        Flag(R.drawable.flag_paraguai, "PARAGUAI"),
+    )
+
+    private lateinit var shuffledFlags: List<Flag>
+    private var currentIndx = 0
+    private var points = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
